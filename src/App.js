261d9e18a20d14dbcdc6c862696e8fefb3670079
Pedro Leite <contato.pedro77@gmail.com>
2023-01-07 03:21:33 -0300

    Pagina post individual

--- src/App.js
+++ src/App.js
@@ -17,10 +17,20 @@ import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import CreatePost from './pages/CreatePost/CreatePost';
 import Dashboard from './pages/Dashboard/Dashboard';
+import Search from './pages/Search/Search';
+import Post from './pages/Post/Post';
 
 // components
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
+
+/*
+  TODO:
+  [] colocar os posts lado a lado na HOME
+  [] fazer um editor wysiwyg para o body dos posts
+
+*/
+
 
 
 
@@ -51,6 +61,8 @@ function App() {
             <Routes>
               <Route path='/' element={<Home />} />
               <Route path='/about' element={<About />} />
+              <Route path='/search' element={<Search />} />
+              <Route path='/posts/:id' element={<Post />} />
               <Route path='/login' element={!user ? <Login /> : <Navigate to="/" />} />
               <Route path='/register' element={!user ? <Register /> : <Navigate to="/" />} />
               <Route path='/posts/create' element={user ? <CreatePost /> : <Navigate to="/login" />}  />
