1c86f2065f8116705a3492ac1329910fdf287a13
Pedro Leite <contato.pedro77@gmail.com>
2023-01-07 02:09:43 -0300

    Pagina Home e component post details

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -76,6 +76,34 @@ export const useAuthentication = () => {
     signOut(auth)
   }
 
+  // login - sign in
+  const login = async(data) => {
+
+    checkIfIsCancelled()
+
+    setLoading(true)
+    setError(false)
+
+    try {
+      await signInWithEmailAndPassword(auth, data.email, data.password)
+      setLoading(false)
+    } catch(error) {
+
+      let systemErrorMessage
+
+      if(error.message.includes("user-not-found")) {
+        systemErrorMessage = "Usuário não encontrado"
+      } else if(error.message.includes("wrong-password")) {
+        systemErrorMessage = "Senha incorreta."
+      } else {
+        systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde."
+      }
+
+      setError(systemErrorMessage)
+      setLoading(false)
+    }
+  }
+
   useEffect(() => {
     return () => setCancelled(true)
   }, [])
@@ -85,6 +113,7 @@ export const useAuthentication = () => {
     createUser,
     error,
     loading,
-    logout
+    logout,
+    login
   }
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,41 @@
 import styles from './Home.module.css'
 
+// hooks
+import { useNavigate, Link } from 'react-router-dom'
+import { useState } from 'react'
+import { useFetchDocuments } from '../../hooks/useFetchDocuments'
+
+
+// components
+import PostDetails from '../../components/PostDetails'
+
 const Home = () => {
+  const [query, setQuery] = useState("")
+  const {documents: posts, loading} = useFetchDocuments("posts")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.home}>
+      <h1>Veja os nossos posts mais recentes</h1>
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input type="text" placeholder='Ou busque por tags' onChange={e => setQuery(e.target.value)} value={query} />
+        <button className='btn btn-dark'>Pesquisar</button>
+      </form>
+      <div>
+        {loading && <p>Carregando...</p>}
+        {posts && posts.map((post) => (
+          <PostDetails post={post} key={post.id}/>
+        ))}
+        {posts && posts.length === 0 && (
+          <div className={styles.nopost}>
+            <p>Não foram encontrados posts</p>
+            <Link className='btn' to="/posts/create">Criar primeiro post</Link>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
